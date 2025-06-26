b893275ee3b2b7dceacde3ae4ec5e301781ee563
MianAhtisham <meharyounes067@gmail.com>
2025-06-26 09:00:08 +0500

    Updated frontend files and added new components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {LoginPage} from "./Routes.js"
+import {SignupPage} from "./Routes.js"
 const App = () => {
   return (
     <div>
-    <p className='text-red-700'>Hello</p>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/sign-up" element={<SignupPage />} />
+    </Routes>
+    </BrowserRouter>
     </div>
   )
 }
