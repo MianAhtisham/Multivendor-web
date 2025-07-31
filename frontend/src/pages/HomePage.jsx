d3101a7ca29310b1a9f18e9c2d98477b8858027a
MianAhtisham <meharyounes067@gmail.com>
2025-07-31 09:41:22 +0500

    Describe your change

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,10 @@ import Categories from "../components/Route/Categories/Categories.jsx";
 import BestDeals from "../components/Route/BestDeals/BestDeals.jsx";
 import FeaturedProduct  from "../components/Route/FeaturedProduct/FeaturedProduct.jsx"
 import Events from "../components/Events/Events.jsx";
+import Sponsored from "../components/Route/Sponsored.jsx";
+import Footer from '../components/Layout/Footer.jsx';
+
+
 
 
 
@@ -17,6 +21,8 @@ const HomePage = () => {
      <BestDeals />
      <FeaturedProduct />
      <Events />
+     <Sponsored />
+     <Footer />
     </div>
   )
 }
