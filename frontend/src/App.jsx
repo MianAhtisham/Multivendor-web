f176dd68cd4ca655badd115f8530a5eafad70199
MianAhtisham <meharyounes067@gmail.com>
2025-08-01 10:37:26 +0500

    Describe your change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {HomePage, LoginPage, SignupPage, EventsPage} from "./Routes.js"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  HomePage,
+  LoginPage,
+  SignupPage,
+  EventsPage,
+  BestSellingPage,
+  ProductsPage,
+} from "./Routes.js";
 const App = () => {
   return (
     <div>
-    <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/sign-up" element={<SignupPage />} />
-    {/* <Route path="/activation/:activation_token" element={<ActivationPage />}/> */}
-              <Route path="/events" element={<EventsPage />} />
-
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignupPage />} />
+          {/* <Route path="/activation/:activation_token" element={<ActivationPage />}/> */}
+          <Route path="/events" element={<EventsPage />} />
+          <Route path="/best-selling" element={<BestSellingPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,11 +3,17 @@ import SignupPage from '../src/pages/SignupPage.jsx';
 // import ActivationPage from '../src/pages/ActivationPage';
 import HomePage from "../src/pages/HomePage";
 import EventsPage from "./pages/EventsPage.jsx"
+import BestSellingPage from "../src/pages/BestSellingPage.jsx"
+import ProductsPage from "../src/pages/ProductsPage.jsx"
+
+
 
 
 export {
   LoginPage,
   SignupPage,
   HomePage,
+  BestSellingPage,
   EventsPage,
+  ProductsPage
 };
